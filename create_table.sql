bc403935c1f3b73f0edfebfbf2803123472564c5
Sahil Malhotra <sahil061626@gmail.com>
2022-06-28 15:01:06 +0530

    create-table

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,7 @@
+CREATE TYPE current_status AS ENUM('employed', 'self-employed', 'unemployed');
+
 CREATE TABLE users(
     full_name VARCHAR(200),
     yearly_salary INT,
-    employment_status varchar(10) NOT NULL CHECK (employment_status IN('employed', 'self-employed', 'unemployed'))
+    employment_status current_status
 );
