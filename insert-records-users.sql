b130d7d02b04d892b543ebcb72a793fa058841a0
Sahil Malhotra <sahil061626@gmail.com>
2022-06-28 21:31:50 +0530

    Insert and Select with gitignore.

---
+++ insert-records-users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (full_name, yearly_salary, employment_status) VALUES
+('Sahil Malhotra',120000,'self-employed');
+
+INSERT INTO users (full_name, yearly_salary, employment_status) VALUES
+('RAM',1500,'employed');
+
+INSERT INTO users (full_name, yearly_salary, employment_status) VALUES
+('ROCKY',0,'unemployed');
